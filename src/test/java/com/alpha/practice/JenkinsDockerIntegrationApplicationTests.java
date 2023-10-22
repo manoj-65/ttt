b8454e08262ec96be9081042297c7a716f476944
manoj-65 <manoj.y@testyantra.in>
2023-10-22 12:57:32 +0530

    starter files

---
+++ src/test/java/com/alpha/practice/JenkinsDockerIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpha.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
